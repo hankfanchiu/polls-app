6840d2e9678e51a70516b6fbd6697f89394947db
Hank Fanchiu <hank@fanchiu.com>
2015-11-12 23:31:53 -0800

    Implemented User#completed_polls method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,46 @@ class User < ActiveRecord::Base
     class_name: "Response",
     primary_key: :id,
     foreign_key: :respondent_id
+
+  def sql_completed_polls
+    Poll.find_by_sql([<<-SQL, id: self.id])
+      SELECT
+        polls.*
+      FROM
+        polls
+      JOIN
+        questions ON questions.poll_id = polls.id
+      JOIN
+        answer_choices ON answer_choices.question_id = questions.id
+      LEFT OUTER JOIN (
+        SELECT
+          *
+        FROM
+          responses
+        WHERE
+          responses.respondent_id = :id
+        ) AS responses ON responses.answer_choice_id = answer_choices.id
+      GROUP BY
+        polls.id
+      HAVING
+        COUNT(DISTINCT questions.id) = COUNT(responses.id)
+    SQL
+  end
+
+  def completed_polls
+    joins_fragment = <<-SQL
+      LEFT OUTER JOIN (
+        SELECT *
+        FROM responses
+        WHERE respondent_id = #{self.id}
+        ) AS responses ON responses.answer_choice_id = answer_choices.id
+    SQL
+
+    Poll
+      .joins('JOIN questions ON questions.poll_id = polls.id')
+      .joins('JOIN answer_choices ON answer_choices.question_id = questions.id')
+      .joins(joins_fragment)
+      .group('polls.id')
+      .having('COUNT(DISTINCT questions.id) = COUNT(responses.id)')
+  end
 end
