f10b3ee7cfb646f7aad09a3ff4ee997b94b7f90e
Hank Fanchiu <hank@fanchiu.com>
2015-11-12 22:05:35 -0800

    Added single-query Response#sibling_responses_improved

--- app/models/response.rb
+++ app/models/response.rb
@@ -23,6 +23,19 @@ class Response < ActiveRecord::Base
     responses.where(':id IS NULL OR responses.id != :id', id: self.id)
   end
 
+  def sibling_responses_improved
+    binds = { answer_choice_id: self.answer_choice_id, id: self.id }
+    self.class.find_by_sql([<<-SQL, binds])
+      SELECT responses.*
+      FROM answer_choices
+      JOIN questions ON questions.id = answer_choices.question_id
+      JOIN answer_choices AS ac2 ON ac2.question_id = questions.id
+      JOIN responses ON responses.answer_choice_id = ac2.id
+      WHERE answer_choices.id = :answer_choice_id
+      AND (:id IS NULL OR responses.id != :id)
+    SQL
+  end
+
   private
   def respondent_has_not_already_answered_question
     if sibling_responses.exists?(respondent_id: self.respondent_id)
